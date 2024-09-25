a4cbec871e27940ff1977c81250b076772346452
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-09-25 20:03:08 +0300

    feat: add header component

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const navMenu = document.getElementById('nav-menu');
+const navLinks = document.querySelectorAll('.nav-link');
+const hamburger = document.getElementById('hamburger');
+
+const toggleMenu = () => {
+  navMenu.classList.toggle('left-[0]');
+  hamburger.classList.toggle('ri-close-large-line');
+};
+
+hamburger.addEventListener('click', toggleMenu);
+
+navLinks.forEach((link) => link.addEventListener('click', toggleMenu));
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
 
